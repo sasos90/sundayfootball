8db44bff92fb0e211ec3fe833cf81bd7dbbd6d0f
Saso Sabotin <sasos90@gmail.com>
2017-04-20 20:35:02 +0200

    Name, description, ID and version changed

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -4,7 +4,7 @@ import {Environment} from "../models/Environment";
  */
 export class Config {
     public static ENV: Environment = Environment.LIVE;
-    public static VERSION: string = "1.5.0";
+    public static VERSION: string = "0.0.1";
     public static SALT: string = "*k9[unD1LrQSQ2_";
 
     // backend host config
