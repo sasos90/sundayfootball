d2d4259648b03c1509342feb8f45896542f8bc84
Saso Sabotin <sasos90@gmail.com>
2017-04-22 20:54:22 +0200

    Real name shown in the highscores when rank is not top10

--- src/pages/highscore/highscore.ts
+++ src/pages/highscore/highscore.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Backend} from "../../services/Backend";
 import {Device} from "@ionic-native/device";
+import {LSK} from "../../models/LSK";
+import {LocalStorage} from "../../services/LocalStorage";
 
 @Component({
     selector: 'highscore',
@@ -15,6 +17,7 @@ export class HighScore {
     public allTimeHighscores: Array<any> = [];
     public notTop10Rank: any;
     public rankingError: boolean = false;
+    public name: string = LocalStorage.get(LSK.NAME) || "Sunday Team";
 
     constructor(
         public navCtrl: NavController,
